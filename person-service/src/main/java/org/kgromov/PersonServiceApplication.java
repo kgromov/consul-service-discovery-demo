4a141d33c31f2ee3104260f2738e97262f161311
konstantin <konst.gromov@gmail.com>
2026-04-25 03:15:26 +0300

    Try out otlp to observe traces (zipkin still wins context due to transparency)

--- person-service/src/main/java/org/kgromov/PersonServiceApplication.java
+++ person-service/src/main/java/org/kgromov/PersonServiceApplication.java
@@ -1,16 +1,20 @@
 package org.kgromov;
 
 
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.kgromov.model.CreditCard;
 import org.kgromov.model.Person;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,4 +68,20 @@ class PersonController {
                 .retrieve()
                 .body(CreditCard.class);
     }
+}
+
+@Component
+class InstallOpenTelemetryAppender implements InitializingBean {
+
+    private final OpenTelemetry openTelemetry;
+
+    InstallOpenTelemetryAppender(OpenTelemetry openTelemetry) {
+        this.openTelemetry = openTelemetry;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        OpenTelemetryAppender.install(this.openTelemetry);
+    }
+
 }
